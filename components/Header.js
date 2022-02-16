e2ecc2e6194eeb8e500cd1b02be0e4762116e9a5
Hyperworks-wtf <pr@hyperworks.wtf>
2022-02-16 16:00:56 +0100

    Test Img

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,9 @@ const Header = () => (
         <div className=" df-nav-grid">
             <a href="#" className="df-logo"><Image width={"120px"} height={'30px'} src="/images/DentedFeels_Logo.png" loading="lazy" alt="" className="df-logo-img"/></a>
             <div className="df-navbar-content">
-            <a href="https://opensea.io" rel="noreferrer" target="_blank" className="df-btn-small df-btn-none w-inline-block"><Image height={"18px"} width={"18px"} src="/images/opensea.svg" loading="lazy" alt="" className="df-img-small"/></a>
-            <a href="https://discord.com" target="_blank" rel="noreferrer" className="df-btn-small df-btn-none w-inline-block"><Image height={"18px"} width={"18px"} src="/images/discord.svg" loading="lazy" alt="" className="df-img-small"/></a>
-            <a href="https://twitter.com/DentedFeelsNFT" rel="noreferrer" target="_blank" className="df-btn-small df-btn-none w-inline-block"><Image height={"18px"} width={"18px"} src="/images/twitter.svg" loading="lazy" alt="" className="df-img-small"/></a>
+            <a href="https://opensea.io" rel="noreferrer" target="_blank" className="df-btn-small df-btn-none w-inline-block"><img src="/images/opensea.svg" loading="lazy" alt="" className="df-img-small"/></a>
+            <a href="https://discord.com" target="_blank" rel="noreferrer" className="df-btn-small df-btn-none w-inline-block"><img src="/images/discord.svg" loading="lazy" alt="" className="df-img-small"/></a>
+            <a href="https://twitter.com/DentedFeelsNFT" rel="noreferrer" target="_blank" className="df-btn-small df-btn-none w-inline-block"><img src="/images/twitter.svg" loading="lazy" alt="" className="df-img-small"/></a>
             <a  href="https://dentedfeels.com" className="df-btn-menu w-nav-button">
                 <div className="text-menu on">Home</div>
             </a>
